6e1e1b1d7202c8ec2677bae04e326f8bfe36bc13
Trish <roguemacaroon@gmail.com>
2024-02-02 17:59:24 -0400

    renamed classes, added js folder, renamed js

--- javascript/header-script.js
+++ javascript/header-script.js
@@ -1,5 +1,5 @@
 const hamburger = document.querySelector('.hamburger');
-const navLink = document.querySelector('.nav__link');
+const navLink = document.querySelector('.nav-link');
 
 hamburger.addEventListener('click', () => {
   navLink.classList.toggle('hide');
